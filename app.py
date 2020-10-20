6581bfb41fbfe8ac8e28e067f6e6495fe12cb1cd
ztangaj <ztangaj@connect.ust.hk>
2020-10-21 03:54:10 +0800

    aa

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask , render_template, request, Response, jsonify, abort
-import requests
+#import requests
 import json
 
 app = Flask(__name__)
@@ -13,25 +13,25 @@ def home():
 def about():
     return  render_template('about.html')
 
-@app.route('/panel',methods = ['GET','POST'])
-def panel():
-    if request.method == 'POST':
-        if request.form['submit_button']== '開燈':
-            print("Open")
-            url = 'http://zonicspaceworkshop.com/Iot/Light'
-            data = {'Switch':'Open'}
-            headers = {'Content-Type': 'application/json'}
-            requests.post(url, data=json.dumps(data),headers = headers)
-            return render_template('panel.html')
-        elif request.form['submit_button']=='關燈':
-            url = 'http://zonicspaceworkshop.com/Iot/Light'
-            data = {'Switch':'Close'}
-            headers = {'Content-Type': 'application/json'}
-            requests.post(url, data=json.dumps(data),headers = headers)
-            return render_template('panel.html')
-            #return ("Close")
-    elif request.method == 'GET':
-        return render_template('panel.html')
+#@app.route('/panel',methods = ['GET','POST'])
+#def panel():
+#    if request.method == 'POST':
+#        if request.form['submit_button']== '開燈':
+#            print("Open")
+#            url = 'http://zonicspaceworkshop.com/Iot/Light'
+#            data = {'Switch':'Open'}
+#            headers = {'Content-Type': 'application/json'}
+#            requests.post(url, data=json.dumps(data),headers = headers)
+#            return render_template('panel.html')
+#        elif request.form['submit_button']=='關燈':
+#            url = 'http://zonicspaceworkshop.com/Iot/Light'
+#            data = {'Switch':'Close'}
+#            headers = {'Content-Type': 'application/json'}
+#            requests.post(url, data=json.dumps(data),headers = headers)
+#            return render_template('panel.html')
+#            #return ("Close")
+#    elif request.method == 'GET':
+#        return render_template('panel.html')
 
 #webhook Iot
 @app.route('/Iot/AC',methods = ['POST','GET'])
